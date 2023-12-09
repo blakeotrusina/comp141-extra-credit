f5d58f3987544c8d4bf6b3366f95812bd6edaa36
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-12-09 05:29:36 +0000

    update

--- main.sh
+++ main.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo "Hello World!"
+mkdir /home/comp141/extra-credit/output
+mv thistext.txt /home/comp141/extra-credit/output
+cd /home/comp141/extra-credit/output
+cat thistext.txt > read.txt
+pwd > pwd.txt
+ls > ls.txt
+cp thistext.txt copy.txt
+alias todaysdate='"%m/%d/%Y"'
+todaysdate > date.txt
+wc -w thistext.txt > textcount.txt
+ps | head -5 > process.txt
+ifconfig | head -5 > netstat.txt
+mount | head -5 > mount.txt
+echo >> permissions.txt
+chmod 777 permissions.txt
+TESTENV1="test"
+cat thistext.txt | grep -E '^\[a-zA-Z]{3,}' > regex.txt
+cd ..
